7519a972e4949ef655c7116875add0255683ce3d
Kitty <kitty@sparkamplify.com>
2018-11-02 14:53:24 +0800

    [ADD] projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+} from 'react-mdl';
 
-const Projects = () => (
-  <div><h1>Projects Page</h1></div>
-);
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    const { activeTab } = this.state;
+    if (activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          {/* project 1 */}
+          <Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover' }}>React Project #1</CardTitle>
+            <CardText>practice</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* project 2 */}
+          <Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover' }}>React Project #2</CardTitle>
+            <CardText>practice</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    }
+    return (
+      <div><h1>This is React</h1></div>
+    );
+  }
+
+  render() {
+    const { activeTab } = this.state;
+    return (
+      <div className="caregory-tabs">
+        <Tabs
+          activeTab={activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>Express</Tab>
+          <Tab>React</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default Projects;
